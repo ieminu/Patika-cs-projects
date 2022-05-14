f15d982378ce523d79d8501037ff87fa15b1ec61
Yemin Elhasan <98277683+ieminu@users.noreply.github.com>
2022-05-14 09:46:57 +0300

    Integer Ikililerin Toplami

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace AlanHesaplama
+namespace IntegerIkililerinToplami
 {
     class Program
     {
@@ -8,38 +8,101 @@ namespace AlanHesaplama
         {
             RunProgram:
 
-            Console.WriteLine("\nÜzerinde işlem yapacağınız şekli seçiniz:");
-            Console.WriteLine("Daire (1)");
-            Console.WriteLine("Üçgen (2)");
-            Console.WriteLine("Kare (3)");
-            Console.WriteLine("Dikdörtgen (4)\n");
+            Console.WriteLine("\nAralarında boşluk bırakarak çift sayıda pozitif sayı giriniz:");
 
-            InputShapeID:
+            Input:
 
-            int inputShapeID = ConsoleManager.GetCorrectInputInt();
+            string input = Console.ReadLine();
 
-            switch (inputShapeID)
+            // Equalize the number of spaces in 'spacesAsLengthOfInput' to input's length {
+            string spacesAsLengthOfInput = "";
+            for (int i = 0; i < input.Length; i++)
+                spacesAsLengthOfInput += ' ';
+            // }
+
+            // Turn the long spaces into one space {
+            for (string spaces = spacesAsLengthOfInput; spaces.Length > 1; spaces = spaces.Remove(0, 1))
             {
-                case 1 :
-                    Shapes.Circle();
-                    break;
-                    
-                case 2 :
-                    Shapes.Triangle();
-                    break;
-                    
-                case 3 :
-                    Shapes.Square();
-                    break;
-                    
-                case 4 :
-                    Shapes.Rectangle();
-                    break;
+                CheckSpaces:
 
-                default :
-                    Console.WriteLine("\nSayınız 1, 2, 3 ve 4 sayıları dışındaki bir değere sahip olamaz! Tekrar sayı giriniz:");
-                    goto InputShapeID;
+                if (input.Contains(spaces))
+                {
+                    input = input.Replace(spaces, " ");
+                    goto CheckSpaces;
+                }
             }
+            // }
+
+            if (input.Length != 0 && input[0] == ' ')
+            {
+                input = input.Remove(0, 1);
+            }
+
+            if (input.Length != 0 && input[input.Length - 1] == ' ')
+            {
+                input = input.Remove(input.Length - 1, 1);
+            }
+
+            float[] numbers = new float[0];
+
+            try
+            {
+                string[] stringNumbers = input.Split(" ");
+                numbers = new float[stringNumbers.Length];
+
+                for (int i = 0; i < stringNumbers.Length; i++)
+                {
+                    numbers[i] = float.Parse(stringNumbers[i]);
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("\nSayı dışında bir değer giremezsiniz! Tekrar giriniz:");
+                goto Input;
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("\n'{0}' sayısından büyük veya negatif bir sayı giremezsiniz! Tekrar giriniz:", Math.Sqrt(float.MaxValue) / 2);
+                goto Input;
+            }
+
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("\nBoş değer giremezsiniz! Tekrar giriniz:");
+                goto Input;
+            }
+
+            if (numbers.Length % 2 == 1)
+            {
+                Console.WriteLine("\nTek sayıda sayı giremezsiniz! Lütfen çift sayıda sayı giriniz:");
+                goto Input;
+            }
+
+            foreach (float number in numbers)
+            {
+                if (number > Math.Sqrt(float.MaxValue) / 2)
+                {
+                    Console.WriteLine("\n'{0}' sayısından büyük bir sayı giremezsiniz! Tekrar giriniz:", Math.Sqrt(float.MaxValue) / 2);
+                    goto Input;
+                }
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i += 2)
+            {
+                if (numbers[i] != numbers[i + 1])
+                {
+                    Console.Write(numbers[i] + numbers[i + 1] + " ");
+                }
+
+                else
+                {
+                    Console.Write(Math.Pow(numbers[i] + numbers[i + 1], 2) + " ");
+                }
+            }
+            Console.WriteLine();
 
             Console.WriteLine("\nYeniden denemek için 'y' giriniz:");
             string inputKey = Console.ReadLine();
